7e977f78ac778d1b0741bb7863d30643087bcc05
Timo Drick <t.drick@apps-on-air.de>
2021-03-24 05:21:26 +0100

    Fixed codestyle issues.

--- app/src/main/java/com/example/androiddevchallenge/BaseCanvasAnimations.kt
+++ app/src/main/java/com/example/androiddevchallenge/BaseCanvasAnimations.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import androidx.compose.ui.geometry.Offset
@@ -85,7 +100,7 @@ fun DrawScope.RainCloud(seconds: Double, wind: Float) {
     canvas.restore()
 }
 
-fun DrawScope.Rain(seconds: Double, xOffset: Float, speed: Float, wind: Float)  {
+fun DrawScope.Rain(seconds: Double, xOffset: Float, speed: Float, wind: Float) {
     val yPos = (((seconds * 100.0 * speed) % 100) / 50f).toFloat()
 
     val direction = Vec2(2f * wind, 1f).normalized
@@ -99,11 +114,11 @@ fun smoothstep1(x: Float) = x * x * (3f - 2f * x)
 fun mix(a: Float, b: Float, x: Float) = (1f - x) * a + x * b
 fun fract(a: Float): Float = a - floor(a)
 
-fun noise1(a: Float): Float = fract(sin(a * 100f) *5647f)
+fun noise1(a: Float): Float = fract(sin(a * 100f) * 5647f)
 fun noise21(a: Float, b: Float): Float = fract(sin(a * 100f + b * 6574f) * 5647f)
 
 fun fract(a: Double): Double = a - floor(a)
-fun noise1(a: Double): Double = fract(sin(a * 100f) *5647f)
+fun noise1(a: Double): Double = fract(sin(a * 100f) * 5647f)
 
 fun smoothNoise(time: Float): Float {
     val fract = fract(time)
